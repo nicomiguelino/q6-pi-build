dde9db4c535fd573bfc6cf7ec1ac2fcc29b2cca6
nicomiguelino <nicomiguelino2014@gmail.com>
2024-08-07 03:16:01 +0000

    fix: add a step for compiling the toolchain

--- scripts/build_qt6.sh
+++ scripts/build_qt6.sh
@@ -3,7 +3,12 @@
 set -euo pipefail
 
 
-function install_cross_build_tools() {
+# Enable script debugging if the DEBUG environment variable is set and non-zero.
+if [ "${DEBUG:-0}" -ne 0 ]; then
+    set -x
+fi
+
+function download_toolchain() {
     mkdir -p cross-tools && cd cross-tools
 
     BINUTILS_PATH=binutils-2.40.tar.gz
@@ -25,13 +30,72 @@ function install_cross_build_tools() {
         tar xf $GCC_PATH
     fi
 
-    git clone --depth=1 https://github.com/raspberrypi/linux
+    if [ ! -d "./linux" ]; then
+        git clone --depth=1 https://github.com/raspberrypi/linux
+    fi
+}
+
+function compile_toolchain() {
+    cd /build/cross-tools/linux/
+    KERNEL=kernel8
+    make ARCH=arm64 INSTALL_HDR_PATH=/opt/cross-pi-gcc/aarch64-linux-gnu headers_install
+
+    cd ../
+    mkdir -p build-binutils && cd build-binutils
+    ../binutils-2.40/configure --prefix=/opt/cross-pi-gcc --target=aarch64-linux-gnu --with-arch=armv8 --disable-multilib
+    make -j10
+    make install
+    echo "Binutils done"
+
+    cd ../
+    sed -i '66a #ifndef PATH_MAX\n#define PATH_MAX 4096\n#endif' /build/cross-tools/gcc-12.2.0/libsanitizer/asan/asan_linux.cpp
+
+    mkdir -p build-gcc && cd build-gcc
+    ../gcc-12.2.0/configure --prefix=/opt/cross-pi-gcc --target=aarch64-linux-gnu --enable-languages=c,c++ --disable-multilib
+    make -j10 all-gcc
+    make install-gcc
+    echo "Compile glibc partly"
+
+    cd ../
+    mkdir -p build-glibc && cd build-glibc
+    ../glibc-2.36/configure \
+        --prefix=/opt/cross-pi-gcc/aarch64-linux-gnu \
+        --build=$MACHTYPE \
+        --host=aarch64-linux-gnu \
+        --target=aarch64-linux-gnu \
+        --with-headers=/opt/cross-pi-gcc/aarch64-linux-gnu/include \
+        --disable-multilib \
+        libc_cv_forced_unwind=yes
+    make install-bootstrap-headers=yes install-headers
+    make -j10 csu/subdir_lib
+    install csu/crt1.o csu/crti.o csu/crtn.o /opt/cross-pi-gcc/aarch64-linux-gnu/lib
+    aarch64-linux-gnu-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o /opt/cross-pi-gcc/aarch64-linux-gnu/lib/libc.so
+    touch /opt/cross-pi-gcc/aarch64-linux-gnu/include/gnu/stubs.h
+    echo "Build gcc partly"
+
+    cd ../build-gcc/
+    make -j10 all-target-libgcc
+    make install-target-libgcc
+    echo "build complete glibc"
+
+    cd ../build-glibc/
+    make -j10
+    make install
+    echo "build complete gcc"
+
+    cd ../build-gcc/
+    make -j10
+    make install
+    echo "Is finished"
 }
 
 function main() {
-    install_cross_build_tools
+    download_toolchain
+
     mkdir -p /opt/cross-pi-gcc
     export PATH=/opt/cross-pi-gcc/bin:$PATH
+
+    compile_toolchain
 }
 
 main
