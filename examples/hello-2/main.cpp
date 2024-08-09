b13caf83c6c5253fd83982b899b4bbae98acb048
nicomiguelino <nicomiguelino2014@gmail.com>
2024-08-09 20:19:05 +0000

    chore: update the second hello example

--- examples/hello-2/main.cpp
+++ examples/hello-2/main.cpp
@@ -5,7 +5,12 @@ int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   app.setOverrideCursor(Qt::BlankCursor);
 
-  QLabel label("It works!");
+  QString message = qgetenv("MESSAGE");
+  QString text = QString("%1").arg(
+    !message.isNull() ? message : "Lorem ipsum dolor sit amet..."
+  );
+
+  QLabel label(text);
   label.setAlignment(Qt::AlignCenter);
   label.setStyleSheet("font-size: 50px");
   label.show();
