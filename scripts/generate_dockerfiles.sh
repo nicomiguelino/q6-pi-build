17f9539af0fe86a7996b210482006ad6f8c3d9fc
nicomiguelino <nicomiguelino2014@gmail.com>
2024-08-12 11:58:04 -0700

    feat: add support for Raspberry Pi 5

--- scripts/generate_dockerfiles.sh
+++ scripts/generate_dockerfiles.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 export BASE_IMAGE_TAG="bookworm"
 export TARGET_DEVICE=${TARGET_DEVICE:-"x86"}
 
-if [[ ! $TARGET_DEVICE =~ ^(pi3|pi4|x86)$ ]]; then
+if [[ ! $TARGET_DEVICE =~ ^(pi3|pi4|pi5|x86)$ ]]; then
     echo "Invalid target device: $TARGET_DEVICE"
-    echo "Supported devices: pi3, pi4, x86"
+    echo "Supported devices: pi3, pi4, pi5, x86"
     exit 1
 fi
 
@@ -16,6 +16,8 @@ function main() {
         export BASE_IMAGE_NAME="balenalib/raspberrypi3-debian"
     elif [[ "$TARGET_DEVICE" == "pi4" ]]; then
         export BASE_IMAGE_NAME="balenalib/raspberrypi4-64-debian"
+    elif [[ "$TARGET_DEVICE" == "pi5" ]]; then
+        export BASE_IMAGE_NAME="balenalib/raspberrypi5-debian"
     elif [[ "$TARGET_DEVICE" == "x86" ]]; then
         export BASE_IMAGE_NAME="balenalib/intel-nuc-debian"
     fi
