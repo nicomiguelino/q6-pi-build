01c4f6ac5dc2675eeab283947404fe52ae6a08a8
nicomiguelino <nicomiguelino2014@gmail.com>
2024-10-02 17:45:32 +0000

    fix: tweak Pi setup script

--- scripts/generate_dockerfiles.sh
+++ scripts/generate_dockerfiles.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export BASE_IMAGE_TAG="bookworm"
-export TARGET_DEVICE=${TARGET_DEVICE:-"x86"}
+export TARGET_DEVICE=${TARGET_DEVICE:-"pi4"}
 
 if [[ ! $TARGET_DEVICE =~ ^(pi3|pi4|pi5|x86)$ ]]; then
     echo "Invalid target device: $TARGET_DEVICE"
@@ -15,7 +15,7 @@ function main() {
     if [[ "$TARGET_DEVICE" == "pi3" ]]; then
         export BASE_IMAGE_NAME="balenalib/raspberrypi3-debian"
     elif [[ "$TARGET_DEVICE" == "pi4" ]]; then
-        export BASE_IMAGE_NAME="balenalib/raspberrypi4-64-debian"
+        export BASE_IMAGE_NAME="balenalib/raspberrypi3-debian"
     elif [[ "$TARGET_DEVICE" == "pi5" ]]; then
         export BASE_IMAGE_NAME="balenalib/raspberrypi5-debian"
     elif [[ "$TARGET_DEVICE" == "x86" ]]; then
